34a09ec9404068d208ce5e5cba632a2ca42d6ae5
tenshi <sairi.na.tenshi@gmail.com>
2010-10-17 20:36:53 +0400

    master

--- init.cmd
+++ init.cmd
@@ -1,1 +1,1 @@
-git init && git pull git://github.com/sairi-na-tenshi/wc_std-lang-bb.git && git submodule init && git submodule update
+git init && git pull git://github.com/sairi-na-tenshi/wc_std-lang-bb.git master && git submodule init && git submodule update
